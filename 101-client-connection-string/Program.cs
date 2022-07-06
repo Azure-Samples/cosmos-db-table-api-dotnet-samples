f8df602def73b816b1a669a5b4fdd27ad98a47d0
Sidney Andrews <sidandrews@microsoft.com>
2022-07-06 17:42:06 +0000

    Initial commit

---
+++ 101-client-connection-string/Program.cs
@@ -0,0 +1,14 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+// <using_directives> 
+using Azure.Data.Tables;
+// </using_directives>
+
+// <connection_string> 
+// New instance of TableServiceClient class using a connection string
+TableServiceClient client = new(
+    connectionString: Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING")!
+);
+// </connection_string>
